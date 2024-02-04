8f6d8f15adc76b4df92c001b0389701c5d1106c3
ninkatcharawan <102357990+Ninkatcharawan@users.noreply.github.com>
2024-02-04 03:17:50 +0000

    Inserted the data from JSON files

--- 01-data-modeling-i/etl.py
+++ 01-data-modeling-i/etl.py
@@ -89,7 +89,6 @@ def process(cur, conn, filepath):
                 conn.commit()
 
 
-
 def main():
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
